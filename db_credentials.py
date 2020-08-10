9232e8a73c13520e7fce700949c086e1c8b92bd9
adamslou <ladams321@hotmail.com>
2020-08-10 13:34:04 +0900

    Converted DB to MariaDB.

--- db_credentials.py
+++ db_credentials.py
@@ -4,13 +4,13 @@
 # the following will be used by the db_connector.py and 
 # in turn by all the Python code in this codebase to interact with the database
 
-host = 'us-cdbr-east-02.cleardb.com'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
-user = 'b062ec985cb84f' #CHANGE ME
-passwd = '32da5e42' #CHANGE ME
-db = 'heroku_11a29a79a72c19e' #CHANGE ME
-
-# old
-# mysql://bdb7fd1889dbf0:ba130f36@us-cdbr-east-02.cleardb.com/heroku_c981bd63a71b60c?reconnect=true
+host = 'ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+user = 'td1ucl5lttqsnz37' #CHANGE ME
+passwd = 'rrk3qxc9qd9crxt2' #CHANGE ME
+db = 'd97bfcojnk4d8s8g' #CHANGE ME
 
 # new for board-games-inc
 # mysql://b062ec985cb84f:32da5e42@us-cdbr-east-02.cleardb.com/heroku_11a29a79a72c19e?reconnect=true
+
+# for mariaDB
+# mysql://td1ucl5lttqsnz37:rrk3qxc9qd9crxt2@ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/d97bfcojnk4d8s8g
